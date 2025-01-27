cab75560a6e893c1baeb0af9e04b3dffde3c6100
arpan2004 <arpan2004.AD@gmail.com>
2025-01-27 11:07:38 -0500

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Sidekick.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import droneImage from "../assets/drone-shot.png";
+
+const Sidekick = ({text, para}) => {
+  return (
+    <section
+      className="relative bg-cover bg-center bg-no-repeat min-h-screen"
+      style={{ backgroundImage: `url(${droneImage})` }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center p-4">
+        <h1 className="text-3xl md:text-5xl font-bold text-white mb-4 text-center">
+          {text}
+        </h1>
+        <p className="text-base md:text-lg text-white mb-6 text-center mx-4 py-4 max-w-4xl">
+          {para}
+        </p>
+        <button className="px-6 py-2 bg-white text-red-500 font-bold hover:bg-slate-600 hover:text-white">
+            Contact Us
+        </button>
+      </div>
+    </section>
+  )
+}
+
+Sidekick.propTypes = {
+    text: PropTypes.string.isRequired,
+    para: PropTypes.string,
+};
+
+export default Sidekick
