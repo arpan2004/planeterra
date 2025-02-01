33f268861afe95f7e87a2cf7b5c7869cc1b432be
arpan2004 <arpan2004.AD@gmail.com>
2025-02-01 16:40:10 -0500

    add services

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import AboutPage from "./pages/AboutPage";
 import ScrollToTop from "./components/ScrollToTop";
+import ServicesPage from "./pages/ServicesPage";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,11 @@
+import Sidekick from "../components/Sidekick";
+
+const ServicesPage = () => {
+  return (
+    <>
+      <Sidekick text="Our Services" para="Planeterra was established in 2024 with the vision of providing innovative and cutting-edge solutions for our clients. Our dedicated team of drone operators and project managers delivers rapid, reliable, and precise aerial services tailored to meet diverse project needs. From aerial inspections to mapping and photography, we create efficient, cost-effective, and high-quality solutions to enhance your operations, ensuring accuracy and reliability throughout every phase of the project." />
+    </>
+  )
+}
+
+export default ServicesPage
